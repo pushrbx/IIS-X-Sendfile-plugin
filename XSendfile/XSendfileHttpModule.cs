47e2c374ebc7681bfdccb5a5178362b2f1736c6c
pushrbx <thomace44@gmail.com>
2016-03-16 21:16:10 +0000

    solution upgrade, .gitignore added, .net 4.5

--- XSendfile/XSendfileHttpModule.cs
+++ XSendfile/XSendfileHttpModule.cs
@@ -1,127 +1,122 @@
 ﻿using System;
-using System.Globalization;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Web;
-using System.Web.Hosting;
-using System.Web.Util;
 using System.Configuration;
 using System.IO;
-using System.Runtime.InteropServices;
 
 //
 // This feels hacky
 //
 using Microsoft.Web.Administration;
 
-namespace XSendfile
+namespace XSendFile
 {
-    public class XSendfileHttpModule : IHttpModule
+    public class XSendFileHttpModule : IHttpModule
     {
         public void Init(HttpApplication context)
         {
-            context.EndRequest += new EventHandler(context_EndRequest);
+            context.EndRequest += context_EndRequest;
         }
 
-        private void context_EndRequest(object sender, EventArgs e)
+        private static void context_EndRequest(object sender, EventArgs e)
         {
-            HttpResponse response = HttpContext.Current.Response;
-            HttpRequest request = HttpContext.Current.Request;
+            var response = HttpContext.Current.Response;
+            var request = HttpContext.Current.Request;
 
             //
             // Check for the X-Send headers
             //
-            string filePath = response.Headers.Get("X-Sendfile");
-            if (filePath == null)
-                filePath = response.Headers.Get("X-Accel-Redirect");
+            var filePath = response.Headers.Get("X-Sendfile") ?? response.Headers.Get("X-Accel-Redirect");
 
-            if (filePath != null)
+            if (filePath == null) return;
+            //
+            // Determine the file path and ready the response
+            //
+            if (ConfigurationManager.AppSettings["XSendDir"] != null)
+                filePath = Path.Combine(ConfigurationManager.AppSettings["XSendDir"], filePath);    // if there is a base path set (file will be located above this)
+            else if (ConfigurationManager.AppSettings["XAccelLocation"] != null)
+                filePath = filePath.Replace(ConfigurationManager.AppSettings["XAccelLocation"], ConfigurationManager.AppSettings["XAccelRoot"]);
+
+            response.Clear();                               // Clears output buffer
+            response.Headers.Remove("X-Sendfile");          // Remove unwanted headers
+            response.Headers.Remove("X-Accel-Redirect");
+
+            //
+            // Set the cache policy
+            //
+            switch (ConfigurationManager.AppSettings["XSendCache"])
             {
-                //
-                // Determine the file path and ready the response
-                //
-                if (ConfigurationManager.AppSettings["XSendDir"] != null)
-                    filePath = Path.Combine(ConfigurationManager.AppSettings["XSendDir"], filePath);    // if there is a base path set (file will be located above this)
-                else if (ConfigurationManager.AppSettings["XAccelLocation"] != null)
-                    filePath = filePath.Replace(ConfigurationManager.AppSettings["XAccelLocation"], ConfigurationManager.AppSettings["XAccelRoot"]);
-
-                response.Clear();                               // Clears output buffer
-                response.Headers.Remove("X-Sendfile");          // Remove unwanted headers
-                response.Headers.Remove("X-Accel-Redirect");
-
-
-                //
-                // Set the cache policy
-                //
-                if(ConfigurationManager.AppSettings["XSendCache"] == null)
+                case null:
                     response.Cache.SetCacheability(HttpCacheability.NoCache);
-                else if(ConfigurationManager.AppSettings["XSendCache"] == "Public")
+                    break;
+                case "Public":
                     response.Cache.SetCacheability(HttpCacheability.Public);
-                else
+                    break;
+                default:
                     response.Cache.SetCacheability(HttpCacheability.Private);
+                    break;
+            }
+
+
+            //
+            // Get the file information and set headers appropriately
+            //
+            var file = new FileInfo(filePath);
+            if (!file.Exists)
+            {
+                throw new HttpException(404, "File_does_not_exist");
+            }
+
+            if (filePath[filePath.Length - 1] == '.')
+            {
+                throw new HttpException(404, "File_does_not_exist");
+            }
+
+
+            response.Cache.SetLastModified(file.LastWriteTimeUtc);
+            response.Headers.Remove("Content-Length");
+
+
+            if (!string.IsNullOrEmpty(request.ServerVariables["HTTP_RANGE"]))
+            {
+                //request for chunk
+                RangeDownload(file.FullName, HttpContext.Current);
+            }
+            else
+            {
+                response.AddHeader("Content-Length", file.Length.ToString());
+                response.AppendHeader("Accept-Ranges", "bytes");
 
 
                 //
-                // Get the file information and set headers appropriately
+                // Check if we want to detect the mime type of the current content
                 //
-                FileInfo file = new FileInfo(filePath);
-                if (!file.Exists)
+                if (ConfigurationManager.AppSettings["XSendMime"] == null)
                 {
-                    throw new HttpException(404, "File_does_not_exist");
+                    var staticContentSection = WebConfigurationManager.GetSection(HttpContext.Current, "system.webServer/staticContent");
+                    var staticContentCollection = staticContentSection.GetCollection();
+
+                    var mt =
+                        staticContentCollection.FirstOrDefault(
+                            a =>
+                                string.Equals(a.Attributes["fileExtension"].Value.ToString(), file.Extension,
+                                    StringComparison.CurrentCultureIgnoreCase));
+
+                    response.ContentType = mt != null ? mt.GetAttributeValue("mimeType").ToString() : "application/octet-stream";
                 }
 
-                if (filePath[filePath.Length - 1] == '.')
-                {
-                    throw new HttpException(404, "File_does_not_exist");
-                }
 
-                
-                response.Cache.SetLastModified(file.LastWriteTimeUtc);
-                response.Headers.Remove("Content-Length");
+                //
+                // Set a content disposition if it is not already set by the application
+                //
+                if (response.Headers["Content-Disposition"] == null)
+                    response.AppendHeader("Content-Disposition", "inline;filename=" + file.Name);
 
 
-                if (!String.IsNullOrEmpty(request.ServerVariables["HTTP_RANGE"]))
-                {
-                    //request for chunk
-                    RangeDownload(file.FullName, HttpContext.Current);
-                } else {
-                    response.AddHeader("Content-Length", file.Length.ToString());
-                    response.AppendHeader("Accept-Ranges", "bytes");
-
-
-                    //
-                    // Check if we want to detect the mime type of the current content
-                    //
-                    if (ConfigurationManager.AppSettings["XSendMime"] == null)
-                    {
-                        Microsoft.Web.Administration.ConfigurationSection staticContentSection = WebConfigurationManager.GetSection(HttpContext.Current, "system.webServer/staticContent");
-                        Microsoft.Web.Administration.ConfigurationElementCollection staticContentCollection = staticContentSection.GetCollection();
-
-                        var mt = staticContentCollection.Where(
-                            a => a.Attributes["fileExtension"].Value.ToString().ToLower() == file.Extension.ToLower()
-                            ).FirstOrDefault();
-
-                        if (mt != null)
-                            response.ContentType = mt.GetAttributeValue("mimeType").ToString();
-                        else
-                            response.ContentType = "application/octet-stream";
-                    }
-
-
-                    //
-                    // Set a content disposition if it is not already set by the application
-                    //
-                    if (response.Headers["Content-Disposition"] == null)
-                        response.AppendHeader("Content-Disposition", "inline;filename=" + file.Name);
-
-
-                    //
-                    //  Send the file without loading it into memory
-                    //
-                    response.TransmitFile(file.FullName);
-                }
+                //
+                //  Send the file without loading it into memory
+                //
+                response.TransmitFile(file.FullName);
             }
         }
 
@@ -130,17 +125,16 @@ namespace XSendfile
         //
         // http://blogs.visigo.com/chriscoulson/easy-handling-of-http-range-requests-in-asp-net/
         //
-        private void RangeDownload(string fullpath, HttpContext context)
+        private static void RangeDownload(string fullpath, HttpContext context)
         {
             long size, start, end, length, fp = 0;
 
-            using (StreamReader reader = new StreamReader(fullpath))
+            using (var reader = new StreamReader(fullpath))
             {
-         
+
                 size = reader.BaseStream.Length;
                 start = 0;
                 end = size - 1;
-                length = size;
                 // Now that we've gotten so far without errors we send the accept range header
                 /* At the moment we only support single ranges.
                  * Multiple ranges requires some more work to ensure it works correctly
@@ -157,22 +151,21 @@ namespace XSendfile
                 // header('Accept-Ranges: bytes');
                 // multipart/byteranges
                 // http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2         
-                long anotherStart = start;
-                long anotherEnd = end;
-                string[] arr_split = context.Request.ServerVariables["HTTP_RANGE"].Split(new char[] { Convert.ToChar("=") });
-                string range = arr_split[1];
-     
+                long anotherStart;
+                var anotherEnd = end;
+                var arrSplit = context.Request.ServerVariables["HTTP_RANGE"].Split(new char[] { Convert.ToChar("=") });
+                var range = arrSplit[1];
+
                 // Make sure the client hasn't sent us a multibyte range
-                if (range.IndexOf(",") > -1)
+                if (range.IndexOf(",", StringComparison.Ordinal) > -1)
                 {
                     // (?) Shoud this be issued here, or should the first
                     // range be used? Or should the header be ignored and
                     // we output the whole content?
                     context.Response.AddHeader("Content-Range", "bytes " + start + "-" + end + "/" + size);
                     throw new HttpException(416, "Requested Range Not Satisfiable");
-     
                 }
-     
+
                 // If the range starts with an '-' we start from the beginning
                 // If not, we forward the file pointer
                 // And make sure to get the end byte if spesified
@@ -183,10 +176,10 @@ namespace XSendfile
                 }
                 else
                 {
-                    arr_split = range.Split(new char[] { Convert.ToChar("-") });
-                    anotherStart = Convert.ToInt64(arr_split[0]);
+                    arrSplit = range.Split(new char[] { Convert.ToChar("-") });
+                    anotherStart = Convert.ToInt64(arrSplit[0]);
                     long temp = 0;
-                    anotherEnd = (arr_split.Length > 1 && Int64.TryParse(arr_split[1].ToString(), out temp)) ? Convert.ToInt64(arr_split[1]) : size;
+                    anotherEnd = (arrSplit.Length > 1 && Int64.TryParse(arrSplit[1].ToString(), out temp)) ? Convert.ToInt64(arrSplit[1]) : size;
                 }
                 /* Check the range and make sure it's treated according to the specs.
                  * http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
@@ -196,13 +189,13 @@ namespace XSendfile
                 // Validate the requested range and return an error if it's not correct.
                 if (anotherStart > anotherEnd || anotherStart > size - 1 || anotherEnd >= size)
                 {
-     
+
                     context.Response.AddHeader("Content-Range", "bytes " + start + "-" + end + "/" + size);
                     throw new HttpException(416, "Requested Range Not Satisfiable");
                 }
                 start = anotherStart;
                 end = anotherEnd;
-     
+
                 length = end - start + 1; // Calculate new content length
                 fp = reader.BaseStream.Seek(start, SeekOrigin.Begin);
                 context.Response.StatusCode = 206;
